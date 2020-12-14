7cb48bd9c38dc2c28ad3da0dc68fd0964d99fcf2
Toghrul Azizli <togrul.azizli@gmail.com>
2020-12-14 14:53:42 +0100

    Initial commit

---
+++ deckofcards.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6&"
+
+payload={}
+headers = {
+  'Accept': 'application/json',
+  'Cookie': '__cfduid=d7ec628c2dc9a616344df39b02f85590c1606743496'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+deck = response.json()
+deck_id = deck['deck_id']
+print(deck_id)
